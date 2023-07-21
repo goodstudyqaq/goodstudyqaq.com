52a4c72e812dc186776fbf0e1ff1a7e6ee3203e3
shen <goodstudyQAQ@163.com>
2023-07-21 22:22:17 +0800

    init

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+// import Logo from '@/data/logo.svg'
+// import Logo from '@/data/avatar.png'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
@@ -12,7 +13,7 @@ const Header = () => {
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
             <div className="mr-3">
-              <Logo />
+              <img src={siteMetadata.siteLogo} alt="Logo" width={48} height={48} className="rounded-full" />
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -10,7 +10,7 @@ const siteMetadata = {
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/goodStudyTnT/goodstudytnt.com',
-  siteLogo: '/static/images/avatar.png',
+  siteLogo: '/static/images/avatar.jpg',
   image: '/static/images/avatar.jpg',
 //   socialBanner: '/static/images/twitter-card.png',
   email: 'goodstudyqaq@gmail.com',
